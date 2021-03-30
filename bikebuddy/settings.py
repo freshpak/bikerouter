ee12a5d33d5976b86363d45304787323b3545fe5
epak <test@example.com>
2021-03-30 12:28:01 -0700

    latest commit

--- bikebuddy/settings.py
+++ bikebuddy/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_files'),
+    os.path.join(BASE_DIR, 'static_files/'),
 ]
 
 # Media Files
